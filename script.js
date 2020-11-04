a4f60e31cdf17e7c37803101771098d4eecd1022
Tuukka Heinonen <[tukkis@luukku.com]>
2020-11-04 14:57:42 +0200

    moves

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let boardArr = [];
 let piecesArr = [];
 let helperArr = [8, 7, 6, 5, 4, 3, 2, 1, 0];
 let swtich = false;
+let possibleMoves = [];
 
 function toLetters(num) {
     var mod = num % 26,
@@ -16,16 +17,14 @@ function toLetters(num) {
 function createBoard(){
     for(let column = 8; column > 0; column--){
         let currColumnb = [];
-        let currColumnp = [];
         for (let row = 1; row < 9; row++){
-            currColumnb.push(toLetters(row).toLowerCase() + column);
-            currColumnp.push('-');
             let tile = document.createElement('div');
             (row+column) % 2 === 0 ? tile.style.backgroundColor = "#769656" : tile.style.backgroundColor = "#eeeed2";   
             tile.classList.add('tile');
             tile.id = toLetters(row).toLowerCase() + column;
             tile.addEventListener('click', () => moveTo(tile));
             chessBoard.appendChild(tile);
+            currColumnb.push(tile);
         }
         boardArr.push(currColumnb);
         let side = document.createElement('p');
@@ -61,19 +60,181 @@ function drawPieces(){
 }
 
 function move(e){
-    const piece = piecesArr[helperArr[e.target.dataset.key.split('')[1]]][e.target.dataset.key.split('')[0].charCodeAt(0) - 97];
     e.stopPropagation()
-    swtich != false ? swtich[0] === piece[0] ? swtich = [piece[0], e.target] : console.log('nam') : swtich = [piece[0], e.target];
-    console.log(piece, swtich[1]);
+    const pieceLocation = [Number(e.target.dataset.key.split('')[1]) - 1, e.target.dataset.key.split('')[0].charCodeAt(0) - 97];
+    const piece = piecesArr[helperArr[pieceLocation[0] + 1]][pieceLocation[1]];
+    swtich != false ? swtich[0] === piece[0] ? swtich = [piece[0], e.target] : moveTo(boardArr[helperArr[pieceLocation[0] + 1]][pieceLocation[1]]) : swtich = [piece[0], e.target];
+    possibleMoves.forEach(possible => possible.classList.remove('possible'));
+    possibleMoves = [];
+    calcPossibleMoves(piece,pieceLocation);
+    console.log(piece, swtich[1] ,pieceLocation);
 }
 
 function moveTo(tile){
     if(swtich != false){
-        swtich[1].parentNode.removeChild(swtich[1]);
-        tile.appendChild(swtich[1]);
-        swtich = false;
+        if(possibleMoves.includes(tile)){
+            const pieceToFrom = piecesArr[helperArr[swtich[1].dataset.key.split('')[1]]][swtich[1].dataset.key.split('')[0].charCodeAt(0) - 97];
+            const moveToTile = piecesArr[helperArr[tile.id.split('')[1]]][tile.id.split('')[0].charCodeAt(0) - 97];
+            piecesArr[helperArr[tile.id.split('')[1]]][tile.id.split('')[0].charCodeAt(0) - 97] = pieceToFrom; 
+            piecesArr[helperArr[swtich[1].dataset.key.split('')[1]]][swtich[1].dataset.key.split('')[0].charCodeAt(0) - 97] = moveToTile;
+            swtich[1].parentNode.removeChild(swtich[1]);
+            tile.appendChild(swtich[1]);
+            swtich[1].dataset.key = tile.id;
+            swtich = false;
+            possibleMoves.forEach(possible => possible.classList.remove('possible'));
+            possibleMoves = [];
+        }
     }
-    console.log(tile);
+}
+
+function calcPossibleMoves(piece, pieceLocation){
+    possibleMoves.forEach(possible => possible.classList.remove('possible'));
+    possibleMoves = [];
+    let side;
+    piece[0] === 'w' ? side = 1 : side = -1;
+    switch (piece[1]) {
+
+        case 'p':
+
+            let nextTile = boardArr[helperArr[pieceLocation[0]+1+(1 * side)]][pieceLocation[1]];
+            let yummyTileLeft = boardArr[helperArr[pieceLocation[0]+1+(1 * side)]][pieceLocation[1]-1];
+            let yummyTileRight = boardArr[helperArr[pieceLocation[0]+1+(1 * side)]][pieceLocation[1]+1];
+            if(yummyTileLeft && yummyTileLeft.childNodes.length > 0 && yummyTileLeft.childNodes[0].alt.split('')[0] !== piece[0]){
+                yummyTileLeft.classList.add('possible');
+                possibleMoves.push(yummyTileLeft);
+            } 
+            if (yummyTileRight && yummyTileRight.childNodes.length > 0 && yummyTileRight.childNodes[0].alt.split('')[0] !== piece[0]){
+                yummyTileRight.classList.add('possible');
+                possibleMoves.push(yummyTileRight);
+            }
+
+            if(pieceLocation[0] * side === 1 || pieceLocation[0] * side === -6){
+                for(let i = 1; i < 3; i++){
+                    let currTile = boardArr[helperArr[pieceLocation[0]+1+(i*side)]][pieceLocation[1]];
+                    if(currTile.childNodes.length === 0){
+                        currTile.classList.add('possible');
+                        possibleMoves.push(currTile);
+                    } else {
+                        i = 3;
+                    }
+                }
+            } else { 
+                console.log(yummyTileLeft,yummyTileRight)
+                if(nextTile.childNodes.length === 0){
+                    nextTile.classList.add('possible');
+                    possibleMoves.push(nextTile);
+                }
+
+            }
+
+            break;
+
+        case 'R':
+            
+            for(let i = 0; i < 7; i++){
+                if(!boardArr[helperArr[pieceLocation[0]+1+ i+1]]){                   
+                    i = 8;
+                    break;
+                }
+                let currTile = boardArr[helperArr[pieceLocation[0]+1+ i+1]][pieceLocation[1]];
+                console.log(currTile)
+                if(currTile.childNodes.length === 0){
+                    currTile.classList.add('possible');
+                    possibleMoves.push(currTile);
+                } else if(currTile.childNodes[0].alt.split('')[0] !== piece[0]){
+                    currTile.classList.add('possible');
+                    possibleMoves.push(currTile);
+                    i = 8;
+                } else {
+                    i = 8;
+                }
+            }
+            
+            for(let i = 0; i < 7; i++){
+                if(!boardArr[helperArr[pieceLocation[0]-1-i+1]]){                   
+                    i = 8;
+                    break;
+                }
+                let currTile = boardArr[helperArr[pieceLocation[0]-1 - i + 1]][pieceLocation[1]];
+                if(currTile.childNodes.length === 0){
+                    currTile.classList.add('possible');
+                    possibleMoves.push(currTile);
+                } else if(currTile.childNodes[0].alt.split('')[0] !== piece[0]){
+                    currTile.classList.add('possible');
+                    possibleMoves.push(currTile);
+                    i = 8;
+                } else {
+                    i = 8;
+                }
+            }
+
+            for(let i = 0; i < 7; i++){
+                 if(!boardArr[helperArr[pieceLocation[0]+ 1]][pieceLocation[1] + i + 1]){                   
+                    i = 8;
+                    break;
+                }
+                let currTile = boardArr[helperArr[pieceLocation[0]+ 1]][pieceLocation[1] + i + 1];
+                if(currTile.childNodes.length === 0){
+                    currTile.classList.add('possible');
+                    possibleMoves.push(currTile);
+                } else if(currTile.childNodes[0].alt.split('')[0] !== piece[0]){
+                    currTile.classList.add('possible');
+                    possibleMoves.push(currTile);
+                    i = 8;
+                } else {
+                    i = 8;
+                } 
+            }
+
+            for(let i = 0; i < 7; i++){
+                if(!boardArr[helperArr[pieceLocation[0]+ 1]][pieceLocation[1] - i - 1]){                   
+                   i = 8;
+                   break;
+               }
+               let currTile = boardArr[helperArr[pieceLocation[0]+ 1]][pieceLocation[1] - i - 1];
+               if(currTile.childNodes.length === 0){
+                   currTile.classList.add('possible');
+                   possibleMoves.push(currTile);
+               } else if(currTile.childNodes[0].alt.split('')[0] !== piece[0]){
+                   currTile.classList.add('possible');
+                   possibleMoves.push(currTile);
+                   i = 8;
+               } else {
+                   i = 8;
+               } 
+           }
+
+            break;
+
+        case 'N':
+        
+           let y = 2;
+           let x = 1
+           for(let i = 0, j = 1; i < 8; j++, i++){
+                let newY = x * -1 + 4 * -Math.floor(j/4);
+                x = y - 2 * Math.floor(j/4)
+                y = newY;
+                j > 3 ? j = -1 : '';
+                console.log(boardArr[helperArr[pieceLocation[0]+ 1 + x]] [pieceLocation[1] - y])
+           }
+
+            break;
+
+        case 'B':
+        
+            break;
+
+        case 'Q':
+        
+            break;
+
+        case 'K':
+        
+            break;
+
+        default:
+            break;
+    }
 }
 
 createBoard()
